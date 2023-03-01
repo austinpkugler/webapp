f9213d9b25fb7d2cf2064ffb531ffbed6b8b7c02
austinpkugler <austinpkugler@gmail.com>
2023-03-01 12:04:05 -0800

    Add env file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
+import dotenv
 from flask import Flask
 
+import os
 
+
+dotenv.load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
 
 
 from app import routes
